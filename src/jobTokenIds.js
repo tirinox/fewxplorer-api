e5445f9899546f7cb4df17d3bef4a69d3cbf4a97
Tirinox <developer@tirinox.ru>
2021-10-05 16:17:20 +0300

    Personality decoder

--- src/jobTokenIds.js
+++ src/jobTokenIds.js
@@ -1,5 +1,6 @@
-const {timeout} = require("./util");
+const {timeout, simpleProgression} = require("./util");
 const {FewmanContract} = require("./smartcontract");
+const {decodePersonality} = require("./personality");
 
 
 class JobTokenIds {
@@ -37,11 +38,26 @@ class JobTokenIds {
         console.info('OpenSeaJob stopped.')
     }
 
+    async _test() {
+        const n = await this._contract.readTotalSupply()
+        console.log(`Fewman total supply: ${n}`)
+
+        for(const i of simpleProgression(0, 50)) {
+            const token = await this._contract.getTokenByIndex(i)
+            // console.log(`Token ${i} => ${token}`)
+
+            const personality = await this._contract.getPersonality(token)
+            const personalityDecoded = decodePersonality(token, personality)
+            const pers = JSON.stringify(personalityDecoded)
+            console.log(`Personality of #${token} is  [ ${pers} ]`)
+        }
+    }
+
     async _job() {
         this._isRunning = true
 
-        const n = await this._contract.readTotalSupply()
-        console.log(`Fewman total supply: ${n}`)
+        await this._test()  // fixme: debug
+        return  // todo!
 
         while (this._isRunning) {
             try {
---
+++ src/personality.js
@@ -0,0 +1,129 @@
+const TRAIT_MAP = {
+    0: {
+        name: 'Hair',
+        values: {
+            '0': 'Albino',  // ***
+            '1': 'Bold',  // **
+            '2': 'Red',  // *
+            '3': 'Brown',
+            '4': 'Blonde',
+            '5': 'Black',
+        }
+    },
+    1: {
+        name: 'Eyes',
+        values: {
+            '0': 'Heterochromic',  // ***
+            '1': 'Red',  // **
+            '2': 'Violet',  // *
+            '3': 'Brown',
+            '4': 'Green',
+            '5': 'Blue',
+        }
+    },
+    2: {
+        name: 'Body',
+        values: {
+            '0': 'Giant',  // ***
+            '1': 'Midget',  // **
+            '2': 'Anorexic',  // *
+            '3': 'Regular',
+            '4': 'Fat',
+            '5': 'Athletic',
+        }
+    },
+    3: {
+        name: 'Sexuality',
+        values: {
+            '0': 'Virgin',  // ***
+            '1': 'Whore',  // **
+            '2': 'Pervert',  // *
+            '3': 'Hetero',
+            '4': 'Bisexual',
+            '5': 'Gay/Lesbian',  // todo: detect by gender
+        }
+    },
+    4: {
+        name: 'Intelligence',
+        values: {
+            '0': 'Degen',  // ***
+            '1': 'Genius',  // **
+            '2': 'Psycho',  // *
+            '3': 'Average',
+            '4': 'Smart',
+            '5': 'Stupid',
+        }
+    },
+    5: {
+        name: 'Career',
+        values: {
+            '0': 'President',  // ***
+            '1': 'Sex Worker',  // **
+            '2': 'Scientist',  // *
+            '3': 'Worker',
+            '4': 'Medic',
+            '5': 'Clerk',
+        }
+    },
+    6: {
+        name: 'Curse',
+        values: {
+            '0': 'Pyromania',  // ***
+            '1': 'Blindness',  // **
+            '2': 'Impotence',  // *
+            '3': 'Alcoholic',
+            '4': 'Drug Addict',
+            '5': 'Porn Addict',
+        }
+    },
+    7: {
+        name: "God's gift",
+        values: {
+            '0': 'FOMO',  // ***
+            '1': 'Luck',  // **
+            '2': 'Longevity',  // *
+            '3': 'Empathy',
+            '4': 'Not gifted',
+            '5': 'Leadership',
+        }
+    },
+}
+
+function genderByTokenId(tokenId) {
+    tokenId = +tokenId
+    if (tokenId % 2 === 0) {
+        return 'Female'
+    } else {
+        return 'Male'
+    }
+}
+
+const VALUE_TO_STARS = {
+    0: 3,
+    1: 2,
+    2: 1,
+    3: 0,
+    4: 0,
+    5: 0
+}
+
+function decodePersonality(tokenId, traitArr) {
+    const traitValues = traitArr.map(a => parseInt(a))
+
+    let entities = {}
+    let index = 0
+    for (const traitValue of traitValues) {
+        const traitDesc = TRAIT_MAP[index]
+        const valueName = traitDesc.values[traitValue]
+        entities[traitDesc.name] = valueName
+        ++index
+    }
+    entities['Gender'] = genderByTokenId(tokenId)
+    return entities
+}
+
+module.exports = {
+    decodePersonality,
+    genderByTokenId,
+    TRAIT_MAP
+}
